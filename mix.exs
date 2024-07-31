7554053e3c088d89084a347eaf2616b3415b5c30
Matvey Karpov <test@example.com>
2024-07-31 10:10:25 +0400

    привет привет привет привет привет привет привет привет привет пр

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Payphone.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
+  # Specifies which paths to compile per environment. For test
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
